8a198de2494b778d315c5dce2c73dfad0ff5da2a
Crowdbotics <team@crowdbotics.com>
2024-03-22 07:58:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "code-quest-junior-47210",
+  url: "https://code-quest-junior-47210.botics.co",
+  api: "https://code-quest-junior-47210.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
